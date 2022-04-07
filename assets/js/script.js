32cad5d980213a0ecd0db1627ca8f8305fcf4ddb
mikewolfe83 <mgray2@ualr.edu>
2022-04-07 18:49:57 +0000

    buffaloupdae

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,35 @@
 async function ShowLevel(){
 
-           document.getElementById("DisplayLevel").addEventListener("click", ShowLevel());
+  var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055646,07055660,07055680,07055780&period=P7D&parameterCd=00065&siteStatus=active";
+  var msgObject = await fetch(myURL);
+  var msgJSONText = await msgObject.text();
+  var msg = JSON.parse(msgJSONText);
+  document.getElementById("msg").innerHTML = msgJSONText;
 
-      // constant variable for api url
-     const myUrl1 = 'https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055660&parameterCd=00065&siteStatus=active'; 
+  var dates = [];
+  var values = [];
 
-         //make the AJAX Call
-         var msg1Object = await fetch(myUrl1);
+  /* Site 1 */
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[0].values[0].value.length
+for (i = 0; i < fLen; i++) { 
+    values[i] = msg.value.timeSeries[0].values[0].value[i].value
+	dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime
+}
+var sitename = msg.value.timeSeries[0].sourceInfo.siteName
+var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value
+var sitedescription = msg.value.timeSeries[0].variable.variableDescription
 
-         //Check the Staus
-         if (msg1Object.status >= 200 && msg1Object.status <= 299) {            
-              var msg1JSONText = await msg1Object.text();
-              // Parse the JSON string into an object
-              var msg1 = JSON.parse(msg1JSONText);
-              /* Your code to process the result goes here - 
-                 display the returned message */
-             
-              
-          }
-   
-              // set variables to pull certain values from returned data
+/* Put your code here to display a graph of values and dates */
 
-              var sitename = [];
-              var agencycode = [];
-              var numdays = msg1.results.length;
-              if (numdays > 0){
-                     for (var i = 0; i < numdays; i++) {
-                            /* sitename value */
-                            sitename[i] = msg1.results[i].siteName;
-                            /* agency code value */
-                            agencycode[i] = msg1.results[i].agencyCode;
-                            /* date is in Unix milleseconds - create a temporary date variable */
-                            var numdays = new Date(msg1.results[i].t);
-                            /* extract the date string from the value */
-                            currencydate[i] = msg1.results[i].dateTime;
-                        }
-              }
 
-               /* convert these tables to currency conversion */
-               var sitenametable = "";
-               if (numdays > 0) {
-                     sitenametable = sitenametable + "<table><caption>sitename</caption><tr><th>Date</th><th>agencycode</th></tr>";
-                   for (var i = 0; i < numdays; i++) {
-                     sitenametable = sitename[i] + "<tr><td>" + numdays[i] + "</td><td>" + agencycode[i] + "</td></tr>";
-                   }
-                   sitenametable = sitenametable + "</table>"
-                   document.getElementById("sitenametable").innerHTML = sitenametable;
-               }
+
+
+
+//var ctx = document.getElementById("chartJs");
+
+
+
+
 
 }
